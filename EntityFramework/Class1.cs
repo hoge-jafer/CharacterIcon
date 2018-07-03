05bae14a33d5148bea319d1f275cfe628a9fd4eb
hoge-jafer <zhong39@live.com>
2018-07-03 11:48:55 +0800

    设计基础的类库

---
+++ EntityFramework/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public class Class1
+    {
+    //基础设施层
+    }
+}
